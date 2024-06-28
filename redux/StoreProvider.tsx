c3cd7d7a9f6b6bd70e58e5da41b586d4138b2aea
Roberto Braile <roberto.braile@roberto-braile-bcn.home>
2024-06-28 11:47:43 +0200

    add persistor and rehydrate

--- redux/StoreProvider.tsx
+++ redux/StoreProvider.tsx
@@ -1,10 +1,17 @@
 "use client";
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default Providers;
--- redux/store.ts
+++ redux/store.ts
@@ -1,19 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
 import { podcastApi } from "@/services/podcast";
 import podcastSelectedReducer from "@/redux/features/podcastSlice";
 import episodeReducer from "@/redux/features/episodeSlice";
 
-export const store = configureStore({
-  reducer: {
-    [podcastApi.reducerPath]: podcastApi.reducer,
-    podcastSelectedState: podcastSelectedReducer,
-    episodeState: episodeReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(podcastApi.middleware),
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  [podcastApi.reducerPath]: podcastApi.reducer,
+  podcastSelectedState: podcastSelectedReducer,
+  episodeState: episodeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(podcastApi.middleware),
+});
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
 
